d560ca032c6aa1eb3fcda2d69c4ba9d877a955cb
Preethi <preethiknot1998@gmail.com>
2020-08-22 23:08:01 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+  import { turn } from "./store.js";
+  import Move from "./Blank.svelte";
+  import Blank from "./Blank.svelte";
+  let turn_value;
+
+  turn.subscribe((player) => {
+    turn_value = player;
+  });
+</script>
+
+<style>
+  main {
+    width: 475px;
+    margin: 0 auto;
+    height: 1000px;
+  }
+  .outer-box {
+    display: flex;
+  }
+  button {
+    width: 100%;
+    margin-top: 20px;
+    background-color: lightblue;
+    font-size: 30px;
+  }
+  button:hover {
+    outline: none;
+  }
+</style>
+
+<main>
+  <h1>Tic Tac Toe</h1>
+  <h2>Number of people playing :</h2>
+  <h2>Player {turn_value}</h2>
+  <div class="outer-box">
+    <Move />
+    <Move />
+    <Move />
+
+  </div>
+  <div class="outer-box">
+    <Move />
+    <Move />
+    <Move />
+
+  </div>
+  <div class="outer-box">
+    <Move />
+    <Move />
+    <Move />
+
+  </div>
+  <button>New game</button>
+</main>
---
+++ src/Blank.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { turn } from "./store.js";
+  export let move = "";
+  
+  function update() {
+    if (move == "") {
+      turn.update((turn_value) => {
+        if (turn_value == "player1") {
+          move = "X";
+          turn_value = "player2";
+        } else if (turn_value == "player2") {
+          move = "O";
+          turn_value = "player1";
+        }
+        return turn_value;
+      });
+    }
+  }
+  // function clicked() {
+  //   if (turn == "player1") {
+  //     move = "X";
+  //     turn = "player2";
+  //   } else if (turn == "player2") {
+  //     move = "O";
+  //     turn = "player1";
+  //   }
+  // }
+</script>
+
+<style>
+  .move {
+    border: solid 1px gray;
+    height: 125px;
+    width: 125px;
+    flex: 1;
+    font-size: 100px;
+    text-align: center;
+    line-height: 125px;
+  }
+</style>
+
+<div on:click={update} class="move">{move}</div>
